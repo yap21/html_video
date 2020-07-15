ea9cc57d744c0f82d03d9de7ac8c9ae8353bfc9f
yap21 <yap21@naver.com>
2020-07-15 14:18:11 +0900

    webview video autoplay

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-// Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const video = document.getElementById('video');
-video.src = "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4";
-video.type = "video/mp4";
+var type = "mp4";
+var url = "https://www.w3schools.com/html/mov_bbb." + type;
+var isSoundStatue = true;
+
+const video = document.createElement('video');
+video.poster = "https://cdn3.iconfinder.com/data/icons/complete-set-icons/512/video512x512.png";
+video.src = url;
+video.type = "video/" + type;
 video.autoplay = true;
-video.loop = false;
+video.preload = "auto";
+video.volume = 0;
+video.loop = true;
+video.onclick = setVideoStatue;
+document.body.appendChild(video);
+
+const txt = document.createElement('p');
+document.body.appendChild(txt);
+
+
+setVideoStatue();
+
+var vConsole = new VConsole();
+
+function setVideoStatue(){
+  isSoundStatue = !isSoundStatue;
+  txt.innerText = 'Sound : ' + isSoundStatue;
+  if(isSoundStatue){
+    video.volume = 1;
+  } else {
+    video.volume = 0;
+  }
+  if(video.readyState) video.play();
+}
